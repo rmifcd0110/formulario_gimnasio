9776f229c2aa8fa502c22428991b634fa9ffee4b
Rafael <rafael.ifcd0110@gmail.com>
2024-09-02 17:05:43 +0100

    actualización

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// Evento onsubmit para validar y mostrar un mensaje personalizado
+document.getElementById("formularioGimnasio").onsubmit = function (event) {
+    let nombre = document.getElementById("nombre").ariaValueMax;
+    if(nombre === ""){
+        alert("Por favor, ingrese el nombre completo");
+    } else {
+        alert("Inscripción realiada con éxito!");
+    }
+};
+
+//Evento onfocus en el campo de nombre para cambiar el fondo
+document.getElementById("nombre").onfocus = function () {
+    this.style.backgroundColor = "#e0f7fa"; // Cambia el color de fondo
+};
+
+//Evento onblur para volvercolor original cuando se pierde el foco
+document.getElementById("nombre").onblur = function () {
+    this.style.backgroundColor = ""; //Vuelve al color original
+}
+
